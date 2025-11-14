e3e1d9860d2c8250657817ee91b29a2bc612b3ac
Ernest Petrovich <ernest.petrovic25@gmail.com>
2025-11-14 12:12:54 +0200

    Init API project

---
+++ api/WorkflowBuilder/src/WorkflowBuilder.Api/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace WorkflowBuilder.Api.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApiDependencies(this IServiceCollection services)
+    {
+        return services
+            .AddGeneralServices();
+    }
+
+    private static IServiceCollection AddGeneralServices(this IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddOpenApi();
+        services.AddHealthChecks();
+        services.AddRouting(options => options.LowercaseUrls = true);
+        
+        return services;
+    }
+}
---
+++ api/WorkflowBuilder/src/WorkflowBuilder.Api/Program.cs
@@ -0,0 +1,31 @@
+using System.Reflection;
+using Scalar.AspNetCore;
+using WorkflowBuilder.Api.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+var environment = EnvironmentExtensions.GetEnvironmentName();
+
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables()
+    .AddUserSecrets(Assembly.GetExecutingAssembly());
+
+builder.Services
+    .AddApiDependencies();
+
+var app = builder.Build();
+
+if (!EnvironmentExtensions.IsProductionEnvironment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
