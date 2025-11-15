d471f24467cec685df67e419d261ccf72a34232f
kai <kajus.padelskas@gmail.com>
2025-11-15 18:40:54 +0200

    DocumentDB Config

--- api/WorkflowBuilder/src/WorkflowBuilder.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ api/WorkflowBuilder/src/WorkflowBuilder.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.Security.Cryptography.X509Certificates;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using MongoDB.Driver;
@@ -34,6 +35,17 @@ public static class ServiceCollectionExtensions
       services.AddSingleton<IMongoClient>(serviceProvider =>
       {
         var options = serviceProvider.GetRequiredService<IOptions<MongoDbConnectionOptions>>().Value;
+      
+        var user = Environment.GetEnvironmentVariable("DB_MONGO_USER");
+        var password = Environment.GetEnvironmentVariable("DB_MONGO_PASSWORD");
+        var host  = Environment.GetEnvironmentVariable("DB_MONGO_HOST");
+
+        if (user is not null && password is not null && host is not null)
+        {
+          var documentDbSettings = MongoClientSettings.FromUrl(new MongoUrl($"mongodb://{user}:{password}@{host}:27017/?replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"));
+          return new MongoClient(documentDbSettings);
+        }
+        
         if (string.IsNullOrWhiteSpace(options.ConnectionString))
         {
           throw new InvalidOperationException("MongoDB connection string is not configured.");
@@ -44,7 +56,7 @@ public static class ServiceCollectionExtensions
           throw new InvalidOperationException("Database name is not configured.");
         }
             
-        var clientSettings = MongoClientSettings.FromConnectionString(options.ConnectionString);
+        var clientSettings = MongoClientSettings.FromUrl(new MongoUrl(options.ConnectionString));
         return new MongoClient(clientSettings);
       });
 
@@ -84,5 +96,35 @@ public static class ServiceCollectionExtensions
       services.AddScoped<IExecutionWorkflowRepository, ExecutionWorkflowRepository>();
       
       return services;
+    }
+
+    private static MongoClient GetMongoClient()
+    {
+      var password = Environment.GetEnvironmentVariable("DB_MONGO_PSW") ?? "-";
+      var connectionString = $"mongodb://apiuser:dSYiQK[Foa5bRVTM$4yREY~$kC6Z@workflowdb.cluster-cd8ugy4i616m.eu-north-1.docdb.amazonaws.com:27017/workflow-builder?tls=true&replicaSet=rs0&readpreference=secondaryPreferred";
+      var pathToCaFile = "rds-combined-ca-bundle.pem";
+
+      // ADD CA certificate to local trust store
+      // DO this once - Maybe when your service starts
+      var localTrustStore = new X509Store(StoreName.Root);
+      var certificateCollection = new X509Certificate2Collection();
+      certificateCollection.Import(pathToCaFile);
+      try 
+      {
+        localTrustStore.Open(OpenFlags.ReadWrite);
+        localTrustStore.AddRange(certificateCollection);
+      } 
+      catch (Exception ex) 
+      {
+        Console.WriteLine("Root certificate import failed: " + ex.Message);
+        throw;
+      } 
+      finally 
+      {
+        localTrustStore.Close();
+      }
+
+      var settings = MongoClientSettings.FromUrl(new MongoUrl(connectionString));
+      return new MongoClient(settings);
     }
 }
