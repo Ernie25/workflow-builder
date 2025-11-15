28835737e665334e297d4933458a34351d8787a8
2000kajusp-lgtm <2000.kajusp@gmail.com>
2025-11-15 18:42:01 +0200

    DocumentDB Config
    Co-authored-by: kai <kajus.padelskas@gmail.com>

--- api/WorkflowBuilder/src/WorkflowBuilder.Api/Program.cs
+++ api/WorkflowBuilder/src/WorkflowBuilder.Api/Program.cs
@@ -1,4 +1,6 @@
+using System.Net;
 using System.Reflection;
+using Microsoft.AspNetCore.Server.Kestrel.Https;
 using Scalar.AspNetCore;
 using WorkflowBuilder.Api.Extensions;
 using WorkflowBuilder.Application.Extensions;
@@ -14,17 +16,14 @@ builder.Configuration
     .AddUserSecrets(Assembly.GetExecutingAssembly());
 
 builder.Services
-    .AddApiDependencies()
-    .AddApplicationDependencies()
-    .AddInfrastructureDependencies(builder.Configuration);
+  .AddApiDependencies()
+  .AddApplicationDependencies()
+  .AddInfrastructureDependencies(builder.Configuration);
 
 var app = builder.Build();
 
-if (!EnvironmentExtensions.IsProductionEnvironment())
-{
-    app.MapOpenApi();
-    app.MapScalarApiReference();
-}
+app.MapOpenApi();
+app.MapScalarApiReference();
 
 app.UseHttpsRedirection();
 
